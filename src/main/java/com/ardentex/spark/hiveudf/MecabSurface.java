3f3bde0ce7f890772894c93341b169dc1f16dc4d
Takeshi KIMURA <kittykimura@gmail.com>
2018-09-28 19:48:32 +0900

    fix import path.

--- src/main/java/com/ardentex/spark/hiveudf/MecabSurface.java
+++ src/main/java/com/ardentex/spark/hiveudf/MecabSurface.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentTypeException;
-import org.apache.hadoop.hive.ql.exec.GenericUDF;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters;
