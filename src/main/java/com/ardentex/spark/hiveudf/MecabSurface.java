118589cbe0143fa4f192f9e4d67f41fb7f00571b
Takeshi KIMURA <kittykimura@gmail.com>
2018-09-28 19:51:36 +0900

    fix Util class

--- src/main/java/com/ardentex/spark/hiveudf/MecabSurface.java
+++ src/main/java/com/ardentex/spark/hiveudf/MecabSurface.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentTypeException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDFUtils;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters;
